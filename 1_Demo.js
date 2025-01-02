8d06961755a0c2302397cc856a16d0995000868c
aditya more <your-adiityamore07@email.com>
2025-01-02 20:27:31 +0530

    Initial commit

---
+++ 1_Demo.js
@@ -0,0 +1,14 @@
+// // console.log("Hello Aditya")
+// // console.log(typeof null)
+
+// // let score = "33abc"
+
+// // console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log( typeof valueInNumber)
+// console.log(valueInNumber) //Nan Not A number
+
+
+// ********* OPERATIONS**************
+
+
